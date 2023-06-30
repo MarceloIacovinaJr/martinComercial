84259be2c28ff825d86140cee4c72c9a98845191
Vitchenzo2812 <gvitchenzo@gmail.com>
2023-06-30 19:21:47 -0300

    refactor: text service

--- src/shared/screens/Services/index.tsx
+++ src/shared/screens/Services/index.tsx
@@ -21,14 +21,14 @@ export default function Service() {
       title = "Services";
       description = "We are specialists in tractor spare parts. To request a quote, click on the quote button and one of our specialists will promptly assist you.";
       textsBox = ['Some machines we work on', 'We only work with parts of the following machines']
-      buttonDescription = "Budget"
+      buttonDescription = "Quote"
       break;
     
     case 'Spain':
       title = "Servicios";
       description = "Somos especialistas en repuestos para todo tipo de tractores. Para solicitar una cotización, haga un clic en el botón cotizar y uno de nuestros especialistas lo atenderá a la brevedad.";
       textsBox = ['Algunas máquinas en las que trabajamos', 'Solo trabajamos con partes de las siguientes máquinas']
-      buttonDescription = 'Presupuesto'
+      buttonDescription = 'Cotización'
       break;
   }
 
